9921641f343c95110298729f492826397f8a870a
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-01-17 18:15:38 +0100

    home Nuxtlink to default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>Hello, I am Jal.</h1>
     <nav class="space-x-2">
+      <NuxtLink to="/">home</NuxtLink>
       <NuxtLink to="/bio">bio</NuxtLink>
       <NuxtLink to="/skills">skills</NuxtLink>
       <NuxtLink to="/projects">projects</NuxtLink>

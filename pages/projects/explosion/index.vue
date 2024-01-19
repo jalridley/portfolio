fbe0124a639ce390a356852bfb748884e8f92731
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-01-19 22:35:38 +0100

    temp article with lorem ipsum

--- pages/projects/explosion/index.vue
+++ pages/projects/explosion/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <section>
+  <section class="space-y-6">
     <h1 class="flex justify-center text-3xl font-bold">Explosion page 💥</h1>
-    <EmptyState />
+    <p>
+      <strong
+        >Descriptive text about Explosion AI work and Prodigy Teams:</strong
+      >
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam
+      quos corrupti, odit officia hic non illum, modi labore similique neque
+      temporibus sapiente. Minus qui eos repudiandae perspiciatis ullam
+      necessitatibus.
+    </p>
+    <article class="rounded border border-gray-400 bg-purple-300 p-4">
+      <h2>Empty State Component</h2>
+      <EmptyState />
+      <p>
+        <strong>Descriptive text about Component</strong>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam
+        quos corrupti, odit officia hic non illum, modi labore similique neque
+        temporibus sapiente. Minus qui eos repudiandae perspiciatis ullam
+        necessitatibus.
+      </p>
+    </article>
   </section>
 </template>
 <script setup lang="ts">

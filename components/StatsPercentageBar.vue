8220d576553c79a483053f8517f00568b0854e2d
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-01-22 21:06:45 +0100

    StatsPercentageBar component

---
+++ components/StatsPercentageBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    :style="{
+      background: linearGradient(filteredItems),
+    }"
+  />
+</template>
+<script setup lang="ts">
+import linearGradient from '@/utils/linear-gradient'
+import type { ColorStatItem } from '~/types/index'
+
+interface Props {
+  items: ColorStatItem[]
+}
+
+const props = defineProps<Props>()
+
+const filteredItems = computed(() => {
+  return props.items.filter((item: { value: number }) => item.value > 0)
+})
+console.log(linearGradient(filteredItems.value))
+</script>

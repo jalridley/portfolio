8e1a1abe68c386baf36f25be82e0bf92b1f8dda9
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-01-22 21:09:59 +0100

    StatsPercentageBar, mock stats & labels

--- pages/projects/explosion/index.vue
+++ pages/projects/explosion/index.vue
@@ -21,6 +21,51 @@
         necessitatibus.
       </p>
     </article>
+    <article class="rounded border border-gray-400 bg-purple-300 p-4">
+      <h2>Stats Percentage Bar Component</h2>
+      <div class="flex h-full justify-center">
+        <StatsPercentageBar
+          class="w-2 rounded-l-lg"
+          :items="[
+            {
+              color: 'rgba(5, 150, 105, 0.7)',
+              value: 9,
+            },
+            {
+              color: 'rgba(2, 132, 199, 0.7)',
+              value: 7,
+            },
+            {
+              color: 'rgba(71, 85, 105, 0.7)',
+              value: 3,
+            },
+            {
+              color: 'rgba(100, 116, 139, 0.7)',
+              value: 5,
+            },
+            {
+              color: 'rgba(225, 29, 72, 0.7)',
+              value: 5,
+            },
+          ]"
+        />
+        <div class="ml-2 h-full py-1">
+          <div class="px-4 py-2">Green stat label</div>
+          <div class="px-4 py-2">Blue stat label</div>
+          <div class="px-4 py-2">Dark gray stat label</div>
+          <div class="px-4 py-2">Light gray stat label</div>
+          <div class="px-4 py-2">Red stat label</div>
+        </div>
+      </div>
+
+      <p>
+        <strong>Descriptive text about Component</strong>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam
+        quos corrupti, odit officia hic non illum, modi labore similique neque
+        temporibus sapiente. Minus qui eos repudiandae perspiciatis ullam
+        necessitatibus.
+      </p>
+    </article>
   </section>
 </template>
 <script setup lang="ts">

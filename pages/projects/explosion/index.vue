d7f72b4f37990c289c3c8d6fba8c0701d811514b
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-01-23 23:03:49 +0100

    trash icon to view

--- pages/projects/explosion/index.vue
+++ pages/projects/explosion/index.vue
@@ -143,6 +143,15 @@
             class="h-6 w-16"
           />
         </div>
+        <div
+          class="flex h-11 w-11 shrink-0 items-center justify-center rounded-xl border border-gray-200 bg-white"
+        >
+          <img
+            src="@/assets/icons/explosion/trash.svg"
+            alt="Trash can icon"
+            class="h-6 w-6"
+          />
+        </div>
       </span>
 
       <p>

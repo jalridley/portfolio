4ff305219051ba3803578348de550f5a2d38eaf1
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-01-23 22:17:13 +0100

    actions icon img in view

--- pages/projects/explosion/index.vue
+++ pages/projects/explosion/index.vue
@@ -59,6 +59,26 @@
         necessitatibus.
       </p>
     </article>
+    <article class="rounded border border-gray-400 bg-purple-300 p-4">
+      <h2>Custom Icon Design</h2>
+      <div
+        class="flex h-11 w-11 items-center justify-center rounded-xl border border-gray-200 bg-white"
+      >
+        <img
+          src="@/assets/icons/explosion/actions.svg"
+          alt="Greater-than with underscore icon"
+          aria-hidden="true"
+          class="h-6 w-6"
+        />
+      </div>
+      <p>
+        <strong>Descriptive text about Component</strong>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam
+        quos corrupti, odit officia hic non illum, modi labore similique neque
+        temporibus sapiente. Minus qui eos repudiandae perspiciatis ullam
+        necessitatibus.
+      </p>
+    </article>
   </section>
 </template>
 <script setup lang="ts">

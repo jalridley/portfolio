ff41f93455db27caa786d84bc59dcb35e4c6420a
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-01-23 22:41:47 +0100

    annotation icon in view

--- pages/projects/explosion/index.vue
+++ pages/projects/explosion/index.vue
@@ -82,6 +82,16 @@
             class="h-6 w-6"
           />
         </div>
+        <div
+          class="flex h-11 w-11 items-center justify-center rounded-xl border border-gray-200 bg-white"
+        >
+          <img
+            src="@/assets/icons/explosion/annotation.svg"
+            alt="Annotation icon"
+            aria-hidden="true"
+            class="h-6 w-6"
+          />
+        </div>
       </span>
 
       <p>

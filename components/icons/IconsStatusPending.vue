ee1ae50ed00ae0f9f302dc731e10a594738abacb
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-01-26 21:06:41 +0100

    aria-hidden="true"

--- components/icons/IconsStatusPending.vue
+++ components/icons/IconsStatusPending.vue
@@ -1,5 +1,10 @@
 <template>
-  <svg viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    aria-hidden="true"
+    viewBox="0 0 22 22"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
     <rect x="6" y="0" width="4" height="4" fill="#CBD5E1">
       <animate
         attributeType="CSS"

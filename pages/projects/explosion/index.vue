432300440882c9011afda4750cac7d6d53a141f9
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-01-26 16:32:28 +0100

    imported IconsStatusActive, added item in items prop

--- pages/projects/explosion/index.vue
+++ pages/projects/explosion/index.vue
@@ -185,9 +185,34 @@
         necessitatibus.
       </p>
     </article>
+    <article class="rounded border border-gray-400 bg-purple-300 p-4">
+      <h2>Stats List Component</h2>
+      <StatsList
+        class="h-full"
+        title="Stats"
+        :items="[
+          {
+            icon: IconsStatusActive,
+            stat: 4, // tasksStats.running,
+            name: 'Running',
+            // url: `/project/${projectId}/tasks?q=status:STARTED`,
+            classes: 'text-emerald-600 hover:text-emerald-700',
+          },
+        ]"
+      />
+      <p>
+        <strong>Descriptive text about Component</strong>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam
+        quos corrupti, odit officia hic non illum, modi labore similique neque
+        temporibus sapiente. Minus qui eos repudiandae perspiciatis ullam
+        necessitatibus.
+      </p>
+    </article>
   </section>
 </template>
 <script setup lang="ts">
+import IconsStatusActive from '~/components/icons/IconsStatusActive.vue'
+
 useHead({
   title: 'Explosion Ai',
 })

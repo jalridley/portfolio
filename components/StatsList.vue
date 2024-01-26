ecc8226b0cd3d07a1d0be2aee6119abd015252e5
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-01-26 16:29:49 +0100

    StatsList component, refactored to use component prop

---
+++ components/StatsList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="h-full rounded border border-slate-200 bg-white">
+    <div class="border-b border-slate-200 px-4 py-2 font-medium text-slate-600">
+      {{ title }}
+    </div>
+    <ul class="py-1">
+      <li v-for="(item, index) in filteredItems" :key="index" class="text-sm">
+        <NuxtLink
+          class="group my-1 flex h-full w-full items-center justify-between px-4 py-2 transition"
+          :class="[
+            {
+              'focus-visible:focus-inset cursor-pointer hover:bg-slate-100':
+                item.url,
+            },
+            item.classes,
+          ]"
+          :to="item.url"
+        >
+          <span class="flex items-center">
+            <span class="mr-2 h-3.5 w-3.5">
+              <component :is="item.icon" />
+            </span>
+
+            {{ item.stat }}
+            {{ item.name }}
+          </span>
+          <img
+            v-if="item.url"
+            name="arrow-up"
+            class="ml-2 h-2.5 w-2.5 rotate-90"
+          />
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { DashboardStat } from '~/types/index'
+
+interface Props {
+  title: string
+  items: DashboardStat[]
+}
+
+const props = defineProps<Props>()
+
+const filteredItems = computed(() => {
+  return props.items.filter((item: DashboardStat) => item.stat > 0)
+})
+</script>

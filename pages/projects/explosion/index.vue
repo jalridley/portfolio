8adad97f0919bd616dce7ffc4ac409629002dc5d
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-01-26 20:38:36 +0100

    StatsGrid component to index with WIP items

--- pages/projects/explosion/index.vue
+++ pages/projects/explosion/index.vue
@@ -236,9 +236,40 @@
         necessitatibus.
       </p>
     </article>
+    <article class="rounded border border-gray-400 bg-purple-300 p-4">
+      <h2>Stats Grid Component</h2>
+      <StatsGrid
+        class="lg:order-4"
+        :items="[
+          {
+            name: pluralize(3, 'Action'),
+            stat: 12, // stats.actions_count,
+            icon: 'actions',
+          },
+          {
+            name: 'Running',
+            stat: 9, // actionsStats.running,
+            icon: 'status-active', // actionsStats.running > 0 ? 'status-active' : 'status-paused',
+          },
+          {
+            name: 'Failed',
+            stat: 2, // actionsStats.failed,
+            icon: 'status-error', // actionsStats.failed > 0 ? 'status-error' : 'status-paused',
+          },
+        ]"
+      />
+      <p>
+        <strong>Descriptive text about Component</strong>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam
+        quos corrupti, odit officia hic non illum, modi labore similique neque
+        temporibus sapiente. Minus qui eos repudiandae perspiciatis ullam
+        necessitatibus.
+      </p>
+    </article>
   </section>
 </template>
 <script setup lang="ts">
+import { pluralize } from '@/utils/language'
 import IconsStatusActive from '~/components/icons/IconsStatusActive.vue'
 import IconsStatusLoading from '~/components/icons/IconsStatusLoading.vue'
 import IconsStatusPaused from '~/components/icons/IconsStatusPaused.vue'

8a044db21badc5e99a707b6146f5d3349bae72a7
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-01-27 16:06:57 +0100

    EmptyState replaced slot with component, added default

--- components/EmptyState.vue
+++ components/EmptyState.vue
@@ -2,9 +2,11 @@
   <div
     class="rounded-md border border-gray-200 bg-white px-4 py-12 text-center text-gray-500"
   >
-    <div class="motion-safe:animate-icon-bounce mx-auto h-16 w-16">
-      <slot name="icon"><IconsEmptyEye /></slot>
-    </div>
+    <component
+      :is="icon"
+      class="motion-safe:animate-icon-bounce mx-auto h-16 w-16"
+    />
+
     <h3 v-if="emptyTitle" class="mt-2 font-medium">
       {{ emptyTitle }}
     </h3>
@@ -16,13 +18,16 @@
 </template>
 
 <script setup lang="ts">
+import IconsEmptyEye from '~/components/icons/IconsEmptyEye.vue'
+
 interface Props {
-  icon?: string
+  icon?: Component
   emptyTitle?: string
   emptyText?: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
+  icon: IconsEmptyEye,
   emptyTitle: '',
   emptyText: 'There is nothing to preview.',
 })

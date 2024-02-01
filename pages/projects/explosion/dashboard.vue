dd867d8e5cf282f05c8eadcf36e542cb5a204905
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-01 18:14:38 +0100

    created dashboard route and view

---
+++ pages/projects/explosion/dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <section>
+      <h1 class="flex justify-center text-5xl font-bold">Dashboard page</h1>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Dashboard',
+})
+</script>

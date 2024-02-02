4b9180f04baac6c19f740d12eafa4dfdda13525d
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-02 16:31:33 +0100

    removed bg-gray-300 from main

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,9 @@
 <template>
-  <div
-    class="3xl:w-1/2 container mx-auto h-screen w-full bg-red-100 p-4 2xl:w-3/5"
-  >
+  <div class="container mx-auto h-screen w-full p-4 2xl:w-3/5 3xl:w-1/2">
     <header class="bg-yellow-100 text-right">
       <div>dark mode toggle</div>
     </header>
-    <main class="bg-gray-300">
+    <main>
       <h1 class="bg-blue-200">Hello, I am Jal.</h1>
       <nav class="space-x-2 bg-green-200">
         <NuxtLink to="/">home</NuxtLink>

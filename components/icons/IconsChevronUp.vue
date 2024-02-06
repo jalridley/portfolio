716a7ca923eccd9a71f036923a793baba02535d4
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-06 15:47:12 +0100

    IconsChevronUp component

---
+++ components/icons/IconsChevronUp.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg" fill="none">
+    <g clip-path="url(#a)">
+      <path
+        fill="currentColor"
+        fill-rule="evenodd"
+        d="m6.03 4.86 4.631 4.737 1.398-1.43-5.33-5.452a.973.973 0 0 0-1.398 0L0 8.167l1.398 1.43L6.03 4.86Z"
+        clip-rule="evenodd"
+      />
+    </g>
+  </svg>
+</template>

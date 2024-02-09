36a69aaf52829926898093924eb11c5fdcf1c18d
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-09 20:15:40 +0100

    IconsPlus component

---
+++ components/icons/IconsPlus.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    aria-hidden="true"
+    viewBox="0 0 12 12"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+  >
+    <path
+      fill="currentColor"
+      d="M6 0a.75.75 0 0 0-.75.75v4.5H.75a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5V.75A.75.75 0 0 0 6 0Z"
+    />
+  </svg>
+</template>

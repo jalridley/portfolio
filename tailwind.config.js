dffc648f0c97b478c47d785cea46cf2596fcc463
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-09 20:15:11 +0100

    purple colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,16 @@ export default {
       // => @media (min-width: 1536px) { ... }
     },
     extend: {
+      colors: {
+        purple: {
+          300: '#B3A5F9',
+          400: '#8974ED',
+          500: '#6E55E1',
+          700: '#4622BB',
+          800: '#3702A8',
+          DEFAULT: '#583FCF',
+        },
+      },
       keyframes: {
         'anim-bounce': {
           '0%, 20%, 50%, 80%, 100%': { transform: 'translateY(0)' },

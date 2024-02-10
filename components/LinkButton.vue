3d10f449772c3c36126f24627cce26009da0e76b
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-10 01:51:01 +0100

    added tertiaryButton, focus-visible, iconClass

--- components/LinkButton.vue
+++ components/LinkButton.vue
@@ -1,25 +1,33 @@
 <template>
   <NuxtLink v-bind="$attrs" :to="to" :class="classes" :aria-label="ariaLabel">
-    <component :is="icon" class="mr-2 h-3 w-3 shrink-0" />
+    <component
+      v-if="icon"
+      :is="icon"
+      class="shrink-0"
+      :class="iconClass || 'mr-2 h-3 w-3'"
+    />
     <slot />
   </NuxtLink>
 </template>
 <script setup lang="ts">
 interface Props {
-  variant?: 'primaryButton' | 'secondaryButton'
+  variant?: 'primaryButton' | 'secondaryButton' | 'tertiaryButton'
   to?: string | Record<string, unknown>
-  icon: Component
+  icon?: Component
   ariaLabel?: string
+  iconClass?: string
 }
 
 const props = withDefaults(defineProps<Props>(), {
   variant: 'primaryButton',
   to: undefined,
+  icon: undefined,
   ariaLabel: undefined,
+  iconClass: undefined,
 })
 
 const baseButton =
-  'border flex justify-center items-center space-x-1 font-medium rounded-md transition focus-visible:focus-default px-4 py-2 text-base'
+  'border flex justify-center items-center space-x-1 font-medium rounded-md transition px-4 py-2 text-base focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-500/90'
 
 const primaryButton = [baseButton, 'text-white bg-purple hover:bg-purple-700']
 
@@ -28,9 +36,13 @@ const secondaryButton = [
   'bg-white text-purple-500 border-purple-300 hover:text-purple-800 hover:border-purple-400',
 ]
 
+const tertiaryButton =
+  'flex items-center px-4 py-2 transition cursor-pointer hover:bg-slate-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-blue-500/90'
+
 const variants = {
   primaryButton,
   secondaryButton,
+  tertiaryButton,
 }
 
 const classes = computed(() => [variants[props.variant]])

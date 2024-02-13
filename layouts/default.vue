e273958487bc43598b7f6e4f6628dde039cec57e
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-13 22:38:07 +0100

    container & footer flex classes, min-w

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="container mx-auto h-screen w-full p-4 2xl:w-3/5">
+  <div
+    class="container mx-auto flex min-h-screen min-w-[390px] flex-col overflow-x-auto p-4 2xl:w-3/5"
+  >
     <header class="bg-yellow-100 text-right">
       <div>dark mode toggle</div>
     </header>
-    <main>
+    <main class="flex-grow">
       <h1 class="bg-blue-200">Hello, I am Jal.</h1>
       <nav class="space-x-2 bg-green-200">
         <NuxtLink to="/">home</NuxtLink>
@@ -14,7 +16,7 @@
       </nav>
       <slot />
     </main>
-    <footer class="flex w-full justify-center p-5">
+    <footer class="flex justify-center p-2">
       <div class="text-xs text-slate-500">
         &copy; {{ new Date().getFullYear() }} Jal Ridley. All Rights Reserved.
       </div>

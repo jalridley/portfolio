611a31ce13c6dbd6f80a1cea2a632be500280916
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-19 18:16:31 +0100

    refactored Stats component - removed section

--- components/Stats.vue
+++ components/Stats.vue
@@ -1,15 +1,13 @@
 <template>
-  <section :class="classes">
-    <ul class="mt-2 flex space-x-4 sm:mt-0 sm:space-x-10">
-      <li v-for="item in items" :key="item.name" class="flex items-center">
-        <component :is="item.icon" class="mr-2 h-3 w-3" />
-        <strong class="mr-1 text-base font-medium text-slate-800">{{
-          item.stat
-        }}</strong>
-        <span class="text-slate-500">{{ item.name }}</span>
-      </li>
-    </ul>
-  </section>
+  <ul :class="classes">
+    <li v-for="item in items" :key="item.name" class="flex items-center">
+      <component :is="item.icon" class="mr-2 h-3 w-3" />
+      <strong class="mr-1 text-base font-medium text-slate-800">{{
+        item.stat
+      }}</strong>
+      <span class="text-slate-500">{{ item.name }}</span>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
@@ -17,10 +15,8 @@ import type { StatItem } from '~/types/index'
 
 interface Props {
   items: StatItem[]
-  classes?: string
+  classes: string
 }
 
-withDefaults(defineProps<Props>(), {
-  classes: '',
-})
+defineProps<Props>()
 </script>

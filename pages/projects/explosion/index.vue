81bb75b86cbb05a2719a3158230dce751dca85aa
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-19 18:19:01 +0100

    deleted old Stats code from UI

--- pages/projects/explosion/index.vue
+++ pages/projects/explosion/index.vue
@@ -243,36 +243,6 @@
         </template>
       </Card>
     </div>
-
-    <article class="rounded border border-gray-400 bg-purple-300 p-4">
-      <h2>Stats Component</h2>
-      <Stats
-        classes="ml-0 flex shrink-0 grow-0 items-center justify-center text-slate-500 lg:ml-10"
-        :items="[
-          {
-            name: pluralize(1, 'Admin'),
-            icon: IconsAdministrator,
-            stat: 1,
-          },
-          {
-            name: pluralize(8, 'Dev'),
-            icon: IconsCode,
-            stat: 8,
-          },
-          {
-            name: pluralize(22, 'Annotator'),
-            icon: IconsAnnotation,
-            stat: 22,
-          },
-        ]"
-      />
-      <p>
-        <strong>Descriptive text about Component</strong>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam
-        quos corrupti, odit officia hic non illum, modi labore similique neque
-        temporibus sapiente. Minus qui eos repudiandae perspiciatis ulla.
-      </p>
-    </article>
   </section>
 </template>
 <script setup lang="ts">

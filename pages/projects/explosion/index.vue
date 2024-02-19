068d95441b618d4064defd203b3e57d8e0409b99
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-19 14:37:49 +0100

    StatsGrid old code deleted

--- pages/projects/explosion/index.vue
+++ pages/projects/explosion/index.vue
@@ -215,35 +215,6 @@
     </div>
 
     <article class="rounded border border-gray-400 bg-purple-300 p-4">
-      <h2>Stats Grid Component</h2>
-      <StatsGrid
-        :items="[
-          {
-            name: pluralize(3, 'Action'),
-            stat: 12,
-            icon: IconsActions,
-          },
-          {
-            name: 'Running',
-            stat: 9,
-            icon: IconsStatusActive,
-          },
-          {
-            name: 'Failed',
-            stat: 2,
-            icon: IconsStatusError,
-          },
-        ]"
-      />
-      <p>
-        <strong>Descriptive text about Component</strong>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus veniam
-        quos corrupti, odit officia hic non illum, modi labore similique neque
-        temporibus sapiente. Minus qui eos repudiandae perspiciatis ullam
-        necessitatibus.
-      </p>
-    </article>
-    <article class="rounded border border-gray-400 bg-purple-300 p-4">
       <h2>Stats Component</h2>
       <Stats
         classes="ml-0 flex shrink-0 grow-0 items-center justify-center text-slate-500 lg:ml-10"

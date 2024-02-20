9e0f824994406a7ab38c38fd78efb286f0479618
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-20 17:26:16 +0100

    removed card dark mode

--- components/Card.vue
+++ components/Card.vue
@@ -1,12 +1,10 @@
 <template>
-  <article
-    class="shadow-3xl max-w-sm overflow-hidden rounded-xl bg-white dark:bg-gray-800"
-  >
+  <article class="shadow-3xl max-w-sm overflow-hidden rounded-xl bg-white">
     <div class="h-72">
       <slot />
     </div>
     <div class="px-4 py-2">
-      <h1 class="text-xl font-bold uppercase text-gray-800 dark:text-white">
+      <h1 class="text-xl font-bold uppercase text-gray-800">
         {{ header }}
       </h1>
       <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">

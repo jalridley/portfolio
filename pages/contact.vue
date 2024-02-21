596627ada47ceb1185f6f622ff282dfecfc64434
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-21 17:40:28 +0100

    added github, linkedin, email contact links

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,28 @@
 <template>
   <section>
-    <h1 class="flex justify-center text-3xl font-bold">contact page</h1>
+    <div>
+      <div>
+        <a
+          href="https://github.com/jalridley"
+          class="text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
+          >Github</a
+        >
+      </div>
+      <div>
+        <a
+          href="https://www.linkedin.com/in/jal-ridley"
+          class="text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
+          >LinkedIn</a
+        >
+      </div>
+      <div>
+        <a
+          href="mailto:ridleyjal@gmail.com"
+          class="text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
+          >Email</a
+        >
+      </div>
+    </div>
   </section>
 </template>
 <script setup lang="ts">

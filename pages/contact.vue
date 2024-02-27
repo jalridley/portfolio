5a97da3cc428fbe06b64c013b8bf5214a7a92426
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-27 19:13:44 +0100

    email envelope svg to contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -9,7 +9,7 @@
           <img
             src="/assets/svg/github-mark.svg"
             alt="github logo"
-            class="mr-1 h-4"
+            class="mr-1 h-3.5"
           />Github</a
         >
       </div>
@@ -20,15 +20,19 @@
           ><img
             src="/assets/svg/linkedin.svg"
             alt="github logo"
-            class="mr-1 h-5 w-5"
+            class="mr-1 h-4 w-4"
           />LinkedIn</a
         >
       </div>
       <div>
         <a
           href="mailto:ridleyjal@gmail.com"
-          class="text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
-          >Email</a
+          class="flex items-center text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
+          ><img
+            src="/assets/svg/envelope.svg"
+            alt="github logo"
+            class="mr-1 h-4 w-4"
+          />Email</a
         >
       </div>
     </div>

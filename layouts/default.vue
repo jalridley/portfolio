ecf1240ef69026a98407affa54d63f1d324f3bc2
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-28 17:32:03 +0100

    home icon to nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,10 @@
     <header class="bg-yellow-100">
       <div class="text-right">dark mode toggle</div>
       <h1 class="bg-blue-200">Hello, I am Jal.</h1>
-      <nav class="space-x-2 bg-green-200">
-        <NuxtLink to="/">home</NuxtLink>
+      <nav class="flex items-center space-x-4 bg-green-200">
+        <NuxtLink to="/"
+          ><img src="/assets/svg/home.svg" alt="home icon" class="h-4 w-4"
+        /></NuxtLink>
         <NuxtLink to="/bio">bio</NuxtLink>
         <NuxtLink to="/skills">skills</NuxtLink>
         <NuxtLink to="/projects">projects</NuxtLink>

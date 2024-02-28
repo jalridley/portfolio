5df7124d827ffce0e7d8e1116628f9d973973bcc
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-28 21:17:19 +0100

    sun icon light and moon icon dark to default

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,17 @@
   <div
     class="mx-auto flex min-h-screen max-w-5xl flex-col px-4 sm:px-6 lg:px-8"
   >
-    <header class="bg-yellow-100">
-      <div class="text-right">dark mode toggle</div>
+    <header>
+      <div class="flex justify-end">
+        <span class="flex items-center">
+          <img src="/assets/svg/sun.svg" alt="sun icon" class="h-4 w-4" />
+          light
+        </span>
+        <span class="flex items-center">
+          <img src="/assets/svg/moon.svg" alt="moon icon" class="h-3.5 w-3.5" />
+          dark
+        </span>
+      </div>
       <h1 class="bg-blue-200">Hello, I am Jal.</h1>
       <nav class="flex items-center space-x-4 bg-green-200">
         <NuxtLink to="/"

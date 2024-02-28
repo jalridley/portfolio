46015710c42c23cff37b219bc6bec2f10623d973
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-02-28 15:59:04 +0100

    fixed syntax error with quotes on a tag

--- pages/bio.vue
+++ pages/bio.vue
@@ -5,11 +5,45 @@
       further my music and event planning career, and after twisting, turning,
       growing and evolving, I am now a Front End Developer.
     </p>
-    <p class="mb-5"><strong>New Rock:</strong> Using my indoor time during the pandemic, I began teaching myself programming as a hobby and quickly discovered it gave me a passion similar to writing music. I was hooked. Throwing myself head first in the pursuit of web development as a career, I enrolled in Le Wagon’s Full-Stack Bootcamp, kicking off my journey. After completion and a successful internship, I began working full time as a Front End Developer at <a href=“https://explosion.ai/”>Explosion AI</a> thriving on the exciting challenge of jumping from a non-tech career into the exciting world of machine learning and NLP.  The SaaS app we were building was ambitious and the codebase complex. It was a far cry from beginner friendly and my brain hurt. I absolutely loved it.</p>
-    <p class="mb-5"><strong>Hard Rock:</strong> I’ve worked with Vue.js, Nuxt, Tailwind CSS, Typescript, CSS/HTML, Cypress e2e testing, unit tests with Vitest, Docker containers, GitHub Actions, UI wireframe and custom icon design in Figma, responsive design, UX accessibility, writing docs, and lots of code review. I have experimented with ReactJS while building a personal project with plans to transfer my Vue skills to learning more.</p>
-    <p class="mb-5"><strong>Soft Rock:</strong> I carry with me the skills I have learned along the way from my life experiences before becoming a developer -  problem solving, conflict management, clear communication, the ability to work solo, with a team, see all sides of a story, and use criticism as a tool to improve.</p>
+    <p class="mb-5">
+      <strong>New Rock:</strong>
+      Using my indoor time during the pandemic, I began teaching myself
+      programming as a hobby and quickly discovered it gave me a passion similar
+      to writing music. I was hooked. Throwing myself head first in the pursuit
+      of web development as a career, I enrolled in Le Wagon’s Full-Stack
+      Bootcamp, kicking off my journey. After completion and a successful
+      internship, I began working full time as a Front End Developer at
+      <a
+        href="https://explosion.ai/"
+        class="text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
+        >Explosion AI</a
+      >
+      thriving on the exciting challenge of jumping from a non-tech career into
+      the exciting world of machine learning and NLP. The SaaS app we were
+      building was ambitious and the codebase complex. It was a far cry from
+      beginner friendly and my brain hurt. I absolutely loved it.
+    </p>
+    <p class="mb-5">
+      <strong>Hard Rock:</strong>
+      I’ve worked with Vue.js, Nuxt, Tailwind CSS, Typescript, CSS/HTML, Cypress
+      e2e testing, unit tests with Vitest, Docker containers, GitHub Actions, UI
+      wireframe and custom icon design in Figma, responsive design, UX
+      accessibility, writing docs, and lots of code review. I have experimented
+      with ReactJS while building a personal project with plans to transfer my
+      Vue skills to learning more.
+    </p>
+    <p class="mb-5">
+      <strong>Soft Rock:</strong>
+      I carry with me the skills I have learned along the way from my life
+      experiences before becoming a developer - problem solving, conflict
+      management, clear communication, the ability to work solo, with a team,
+      see all sides of a story, and use criticism as a tool to improve.
+    </p>
 
-  <p class="mb-5"><strong>THE Rock:</strong> Determination. Determination is my driver.  </p>
+    <p class="mb-5">
+      <strong>THE Rock:</strong>
+      Determination. Determination is my driver.
+    </p>
   </section>
 </template>
 <script setup lang="ts">

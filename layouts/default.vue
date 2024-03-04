27b4bf24b690b1e642a892ce0b4cc9ac4adf6f27
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-05 00:16:49 +0100

    nav-hover to elements, active router classes

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,16 +6,16 @@
       class="sticky top-0 mb-3 flex justify-between border-b border-slate-950/10 bg-white p-3 md:mb-8 md:p-4 lg:mb-10 dark:bg-slate-950"
     >
       <div class="flex items-center gap-4 md:gap-8 lg:gap-10">
-        <NuxtLink to="/"
-          ><img src="/assets/svg/home.svg" alt="home icon" class="h-5 w-5"
+        <NuxtLink class="link-hover" to="/"
+          ><img src="/assets/svg/home.svg" alt="home icon" class="h-6 w-6"
         /></NuxtLink>
-        <NuxtLink to="/bio">bio</NuxtLink>
-        <NuxtLink to="/skills">skills</NuxtLink>
-        <NuxtLink to="/projects">projects</NuxtLink>
-        <NuxtLink to="/contact">contact</NuxtLink>
+        <NuxtLink class="nav-hover" to="/bio">journey</NuxtLink>
+        <NuxtLink class="nav-hover" to="/skills">skills</NuxtLink>
+        <NuxtLink class="nav-hover" to="/projects">projects</NuxtLink>
+        <NuxtLink class="nav-hover" to="/contact">contact</NuxtLink>
       </div>
       <span class="flex items-center">
-        <img src="/assets/svg/sun.svg" alt="sun icon" class="mr-1 h-5 w-5" />
+        <img src="/assets/svg/sun.svg" alt="sun icon" class="mr-1 h-6 w-6" />
       </span>
     </nav>
     <main class="flex grow flex-col">
@@ -28,3 +28,8 @@
     </footer>
   </div>
 </template>
+<style lang="postcss" scoped>
+.router-link-exact-active::after {
+  @apply absolute block h-[4px] w-full scale-x-100 bg-yellow-500 transition-none;
+}
+</style>

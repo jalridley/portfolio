614c250aec27706aa63cb8b3a3abeb10018089dd
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-04 18:19:48 +0100

    border, gap to nav, removed dark mode icon until needed

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,27 +3,20 @@
     class="mx-auto flex min-h-screen max-w-5xl flex-col font-raleway sm:mt-6 lg:mt-12"
   >
     <nav
-      class="lg::mb-10 mb-3 flex justify-between bg-green-200 p-3 md:mb-8 md:p-4"
+      class="lg::mb-10 mb-3 flex justify-between border-b border-slate-900/10 p-3 md:mb-8 md:p-4"
     >
-      <div class="flex items-center gap-4">
+      <div class="flex items-center gap-4 md:gap-8 lg:gap-10">
         <NuxtLink to="/"
-          ><img src="/assets/svg/home.svg" alt="home icon" class="h-4 w-4"
+          ><img src="/assets/svg/home.svg" alt="home icon" class="h-5 w-5"
         /></NuxtLink>
         <NuxtLink to="/bio">bio</NuxtLink>
         <NuxtLink to="/skills">skills</NuxtLink>
         <NuxtLink to="/projects">projects</NuxtLink>
         <NuxtLink to="/contact">contact</NuxtLink>
       </div>
-      <div class="flex">
-        <span class="flex items-center">
-          <img src="/assets/svg/sun.svg" alt="sun icon" class="h-4 w-4" />
-          light
-        </span>
-        <span class="flex items-center">
-          <img src="/assets/svg/moon.svg" alt="moon icon" class="h-3.5 w-3.5" />
-          dark
-        </span>
-      </div>
+      <span class="flex items-center">
+        <img src="/assets/svg/sun.svg" alt="sun icon" class="mr-1 h-5 w-5" />
+      </span>
     </nav>
     <main class="flex grow flex-col">
       <slot />

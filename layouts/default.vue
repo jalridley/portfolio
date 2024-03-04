16c2679b79013a682574a67866232f20bcb053e1
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-05 00:58:56 +0100

    renamed class to nav-item

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,13 +4,13 @@
       class="sticky top-0 mb-3 flex justify-between border-b border-slate-950/10 bg-white p-4 sm:mt-6 md:mb-8 lg:mb-10 lg:mt-12 dark:bg-slate-950"
     >
       <div class="flex items-center gap-4 md:gap-8 lg:gap-10">
-        <NuxtLink class="nav-element" to="/"
+        <NuxtLink class="nav-item" to="/"
           ><img src="/assets/svg/home.svg" alt="home icon" class="h-6 w-6"
         /></NuxtLink>
-        <NuxtLink class="nav-element" to="/bio">journey</NuxtLink>
-        <NuxtLink class="nav-element" to="/skills">skills</NuxtLink>
-        <NuxtLink class="nav-element" to="/projects">projects</NuxtLink>
-        <NuxtLink class="nav-element" to="/contact">contact</NuxtLink>
+        <NuxtLink class="nav-item" to="/bio">journey</NuxtLink>
+        <NuxtLink class="nav-item" to="/skills">skills</NuxtLink>
+        <NuxtLink class="nav-item" to="/projects">projects</NuxtLink>
+        <NuxtLink class="nav-item" to="/contact">contact</NuxtLink>
       </div>
       <span class="flex items-center">
         <img src="/assets/svg/sun.svg" alt="sun icon" class="mr-1 h-6 w-6" />

71c4a7c1ba44c869e8614320be9c17f0724e08e6
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-05 17:09:50 +0100

    swapped github, linkedin icons and deleted their svg

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,27 +2,22 @@
   <section>
     <h1 class="font-besley text-2xl">contact page</h1>
     <div>
-      <div>
+      <div class="flex items-center">
+        <Icon name="mdi:github" />
         <a
           href="https://github.com/jalridley"
-          class="flex items-center text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
+          class="text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
         >
-          <img
-            src="/assets/svg/github-black.svg"
-            alt="github logo"
-            class="mr-1 h-3.5"
-          />Github</a
+          Github</a
         >
       </div>
-      <div>
+      <div class="flex items-center">
+        <Icon name="mdi:linkedin:add-fill" />
         <a
           href="https://www.linkedin.com/in/jal-ridley"
-          class="flex items-center text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
-          ><img
-            src="/assets/svg/linkedin.svg"
-            alt="linkedIn logo"
-            class="mr-1 h-4 w-4"
-          />LinkedIn</a
+          class="text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
+        >
+          LinkedIn</a
         >
       </div>
       <div>

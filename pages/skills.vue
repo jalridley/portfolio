3ea7152ddd34ac356cee75e330186226dc38c1e3
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-05 20:59:16 +0100

    skills icons light and dark

--- pages/skills.vue
+++ pages/skills.vue
@@ -2,19 +2,56 @@
   <section>
     <h1 class="font-besley text-2xl">skills page</h1>
     <ul class="mb-5">
-      <li>Javascript</li>
-      <li>Vue.js</li>
-      <li>Nuxt</li>
-      <li>Pinia</li>
-      <li>TailwindCSS</li>
-      <li>Typescript</li>
-      <li>ESLint</li>
-      <li>HTML/CSS/SASS</li>
-      <li>Storybook</li>
-      <li>Cypress</li>
-      <li>Vitest</li>
-      <li>Git/Github</li>
-      <li>Figma</li>
+      <li class="flex">
+        <Icon name="skill-icons:javascript" />
+        Javascript
+      </li>
+      <li class="flex">
+        <Icon name="skill-icons:vuejs-light" /><Icon
+          name="skill-icons:vuejs-dark"
+        />Vue.js
+      </li>
+      <li class="flex">
+        <Icon name="skill-icons:nuxtjs-dark" /><Icon
+          name="skill-icons:nuxtjs-light"
+        />Nuxt
+      </li>
+      <li class="flex">
+        <Icon name="skill-icons:pinia-dark" /><Icon
+          name="skill-icons:pinia-light"
+        />Pinia
+      </li>
+      <li class="flex">
+        <Icon name="skill-icons:tailwindcss-dark" /><Icon
+          name="skill-icons:tailwindcss-light"
+        />TailwindCSS
+      </li>
+      <li class="flex"><Icon name="skill-icons:typescript" />Typescript</li>
+      <li class="flex"><Icon name="skill-icons:html" />HTML</li>
+      <li class="flex"><Icon name="skill-icons:css" />CSS</li>
+      <li class="flex"><Icon name="skill-icons:sass" />SASS</li>
+      <li class="flex">
+        <Icon
+          name="vscode-icons:file-type-storybook"
+          class="rounded-md bg-[#f4f2ed] p-0.5"
+        />Storybook
+      </li>
+      <li class="flex"><Icon name="skill-icons:cypress-light" />Cypress</li>
+      <li class="flex">
+        <Icon name="skill-icons:vitest-dark" /><Icon
+          name="skill-icons:vitest-light"
+        />Vitest
+      </li>
+      <li class="flex">
+        <Icon name="skill-icons:git" /><Icon
+          name="skill-icons:github-dark"
+        /><Icon name="skill-icons:github-light" />Git/Github
+      </li>
+      <li class="flex">
+        <Icon name="skill-icons:figma-dark" /><Icon
+          name="skill-icons:figma-light"
+        />Figma
+      </li>
     </ul>
   </section>
 </template>

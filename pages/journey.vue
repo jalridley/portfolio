36cf1f1c1f3f17d72dd9a65d024e493f8c47b575
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-05 19:48:07 +0100

    refactored journey html hierarchy, deleted bolt svg

--- pages/journey.vue
+++ pages/journey.vue
@@ -1,20 +1,28 @@
 <template>
   <section>
+    <header>
+      <h1 class="mb-5 font-besley text-2xl sm:text-3xl md:text-5xl lg:text-7xl">
+        Journey.
+      </h1>
+    </header>
+    <h2
+      class="flex items-center font-besley text-lg sm:text-xl md:text-2xl lg:text-3xl"
+    >
+      <Icon name="mdi:amplifier" />
+      Musician Gone Web Dev:
+    </h2>
     <p class="mb-5">
-      <strong class="flex items-center font-besley text-2xl">
-        <Icon name="mdi:amplifier" />
-        Musician Gone Web Dev:</strong
-      >
-
       I moved from Toronto to Berlin to further my music and event planning
       career, and after twisting, turning, growing and evolving, I am now a
       Front End Developer.
     </p>
+    <h2
+      class="flex items-center font-besley text-lg sm:text-xl md:text-2xl lg:text-3xl"
+    >
+      <Icon name="heroicons:fire-16-solid" />
+      New Rock:
+    </h2>
     <p class="mb-5">
-      <strong class="flex items-center font-besley text-2xl"
-        ><Icon name="heroicons:fire-16-solid" />
-        New Rock:
-      </strong>
       Using my indoor time during the pandemic, I began teaching myself
       programming as a hobby and quickly discovered it gave me a passion similar
       to writing music. I was hooked. Throwing myself head first in the pursuit
@@ -31,10 +39,12 @@
       building was ambitious and the codebase complex. It was a far cry from
       beginner friendly and my brain hurt. I absolutely loved it.
     </p>
+    <h2
+      class="flex items-center font-besley text-lg sm:text-xl md:text-2xl lg:text-3xl"
+    >
+      <Icon name="heroicons:bolt-20-solid" />Hard Rock:
+    </h2>
     <p class="mb-5">
-      <strong class="flex items-center font-besley text-2xl"
-        ><Icon name="heroicons:bolt-20-solid" />Hard Rock:</strong
-      >
       I’ve worked with Vue.js, Nuxt, Tailwind CSS, Typescript, CSS/HTML, Cypress
       e2e testing, unit tests with Vitest, Docker containers, GitHub Actions, UI
       wireframe and custom icon design in Figma, responsive design, UX
@@ -42,23 +52,25 @@
       with ReactJS while building a personal project with plans to transfer my
       Vue skills to learning more.
     </p>
+    <h2
+      class="flex items-center font-besley text-lg sm:text-xl md:text-2xl lg:text-3xl"
+    >
+      <Icon name="ph:feather-fill" />Soft Rock:
+    </h2>
     <p class="mb-5">
-      <strong class="flex items-center font-besley text-2xl"
-        ><Icon name="ph:feather-fill" />Soft Rock:</strong
-      >
       I carry with me the skills I have learned along the way from my life
       experiences before becoming a developer - problem solving, conflict
       management, clear communication, the ability to work solo, with a team,
       see all sides of a story, and use criticism as a tool to improve.
     </p>
 
-    <p class="mb-5">
-      <strong class="flex items-center font-besley text-2xl">
-        <Icon name="heroicons:rocket-launch-16-solid" />
-        THE Rock:</strong
-      >
-      Determination. Determination is my driver.
-    </p>
+    <h2
+      class="flex items-center font-besley text-lg sm:text-xl md:text-2xl lg:text-3xl"
+    >
+      <Icon name="heroicons:rocket-launch-16-solid" />
+      THE Rock:
+    </h2>
+    <p class="mb-5">Determination. Determination is my driver.</p>
   </section>
 </template>
 <script setup lang="ts">

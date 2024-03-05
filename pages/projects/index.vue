91e9121b1222a8bfc5336489be19e37d66811ea0
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-05 22:29:31 +0100

    pinball wizard project page and route

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,12 +1,20 @@
 <template>
   <section>
     <h1 class="font-besley text-2xl">projects page</h1>
-    <NuxtLink
-      class="hover:text-emerald-700"
-      to="/projects/explosion"
-      aria-label="Read more about Jal's work at Explosion AI"
-      >Explosion Ai</NuxtLink
-    >
+    <div class="flex flex-col">
+      <NuxtLink
+        class="hover:text-emerald-700"
+        to="/projects/explosion"
+        aria-label="Read more about Jal's work at Explosion AI"
+        >Explosion Ai</NuxtLink
+      >
+      <NuxtLink
+        class="hover:text-emerald-700"
+        to="/projects/pinball-wizard"
+        aria-label="Read more about Jal's project Pinball Wizard"
+        >Pinball Wizard</NuxtLink
+      >
+    </div>
   </section>
 </template>
 <script setup lang="ts">
---
+++ pages/projects/pinball-wizard.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="flex justify-between">
+    <h1
+      class="flex flex-col font-besley text-2xl font-bold sm:text-3xl md:text-5xl lg:text-7xl"
+    >
+      Pinball Wizard
+    </h1>
+  </section>
+</template>
+<script setup lang="ts">
+useHead({
+  title: 'Pinball Wizard',
+})
+</script>

8b39b5dbcdc04209133554ac6cdfcd20837193d6
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-06 13:26:35 +0100

    header font-besley

--- components/Card.vue
+++ components/Card.vue
@@ -1,10 +1,10 @@
 <template>
-  <article class="shadow-3xl max-w-sm overflow-hidden rounded-xl bg-white">
+  <article class="max-w-sm overflow-hidden rounded-xl bg-white shadow-3xl">
     <div class="h-72">
       <slot />
     </div>
     <div class="px-4 py-2">
-      <h1 class="text-xl font-bold uppercase">
+      <h1 class="font-besley text-xl font-bold uppercase">
         {{ header }}
       </h1>
       <p class="mt-1">

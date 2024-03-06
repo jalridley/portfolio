099c9c0d461f4e02aa2954caadc759c259e36573
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-06 17:43:12 +0100

    added card with pinball wizard view

--- pages/projects/pinball-wizard.vue
+++ pages/projects/pinball-wizard.vue
@@ -1,10 +1,34 @@
 <template>
-  <section class="flex justify-between">
+  <section>
     <h1
       class="mb-5 flex flex-col font-besley text-2xl font-bold sm:text-3xl md:text-5xl lg:text-7xl"
     >
       Pinball Wizard.
     </h1>
+    <Card header="Pinball Wizard">
+      <NuxtImg
+        src="/pinball-wizard-wip.png"
+        class="aspect-[4/3] h-full w-full"
+        alt="illustration of Jal smiling with short hair styled up"
+        width="1024px"
+        height="795px"
+        sizes="216px md:288px"
+        format="webp"
+        decoding="auto"
+        loading="lazy"
+        ismap
+      />
+
+      <template #text>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+        voluptas itaque ipsum, commodi obcaecati eius vel ab quibusdam! Ipsa
+        iure exercitationem perferendis? Obcaecati ipsam minus iure ducimus
+        natus, quod perspiciatis? Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Atque eius quam assumenda voluptatem quidem quae ullam
+        quo accusantium, voluptatibus vitae amet molestias dolorem illum tempore
+        asperiores dolor labore. Aliquam, quos!
+      </template>
+    </Card>
   </section>
 </template>
 <script setup lang="ts">

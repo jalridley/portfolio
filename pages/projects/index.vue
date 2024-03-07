69d297620491a26459ed935e23ce3949ff2d309c
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-07 23:01:02 +0100

    explosion card to projects

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -8,12 +8,6 @@
     <div class="flex flex-col">
       <NuxtLink
         class="hover:text-emerald-700"
-        to="/projects/explosion"
-        aria-label="Read more about Jal's work at Explosion AI"
-        >Explosion Ai</NuxtLink
-      >
-      <NuxtLink
-        class="hover:text-emerald-700"
         to="/projects/pinball-wizard"
         aria-label="Read more about Jal's project Pinball Wizard"
         >Pinball Wizard</NuxtLink
@@ -31,9 +25,32 @@
         >Plantfam</NuxtLink
       >
     </div>
+    <Card header="Explosion AI">
+      <NuxtLink
+        class="flex h-full w-full items-center justify-center bg-purple text-white hover:text-yellow-500"
+        to="/projects/explosion"
+        aria-label="Read more about Jal's work at Explosion AI"
+      >
+        <ExplosionLogo class="h-72 w-72" />
+      </NuxtLink>
+
+      <template #text>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus voluptas itaque ipsum, commodi obcaecati eius vel ab
+          quibusdam! Ipsa iure exercitationem perferendis? Obcaecati ipsam minus
+          iure ducimus natus, quod perspiciatis? Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Atque eius quam assumenda voluptatem
+          quidem quae ullam quo accusantium, voluptatibus vitae amet molestias
+          dolorem illum tempore asperiores dolor labore. Aliquam, quos!
+        </p>
+      </template>
+    </Card>
   </section>
 </template>
 <script setup lang="ts">
+import ExplosionLogo from '~/components/ExplosionLogo.vue'
+
 useHead({
   title: 'Projects',
 })

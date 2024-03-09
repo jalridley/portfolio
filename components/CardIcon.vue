28bfa9f58694f7e53cb388fcb1d9f1f59c2d8e8f
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-09 19:14:17 +0100

    card icon clor prop

--- components/CardIcon.vue
+++ components/CardIcon.vue
@@ -2,6 +2,7 @@
   <div class="flex flex-col items-center">
     <Icon
       :name="icon"
+      :color="color"
       class="h-24 w-24 sm:h-32 sm:w-32 lg:h-48 lg:w-48"
       :class="iconClasses"
     />
@@ -15,6 +16,7 @@ interface Props {
   icon: string
   title: string
   iconClasses?: string
+  color?: string
 }
 defineProps<Props>()
 </script>

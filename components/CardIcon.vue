a89f6c375eccc1b0d2b360205a41349f8660cb40
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-09 18:40:14 +0100

    deleted all !mr-0 from icons

--- components/CardIcon.vue
+++ components/CardIcon.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col items-center">
     <Icon
       :name="icon"
-      class="!mr-0 h-24 w-24 sm:h-32 sm:w-32 lg:h-48 lg:w-48"
+      class="h-24 w-24 sm:h-32 sm:w-32 lg:h-48 lg:w-48"
       :class="iconClasses"
     />
     <div class="mt-2 text-center text-lg font-medium sm:text-2xl">

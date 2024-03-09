bd31d48a6217c4fe6cef61f01277b587fe289544
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-09 17:50:39 +0100

    CardIcon component

---
+++ components/CardIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Icon
+      :name="icon"
+      class="!mr-0 h-16 w-16 sm:h-28 sm:w-28"
+      :class="iconClasses"
+    />
+    <div class="mt-2 text-center">{{ title }}</div>
+  </div>
+</template>
+<script setup lang="ts">
+interface Props {
+  icon: string
+  title: string
+  iconClasses?: string
+}
+defineProps<Props>()
+</script>

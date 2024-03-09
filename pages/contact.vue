320afed1bbf6101598ee9db4d1f100dc142ba46b
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-09 19:55:29 +0100

    removed grid, exchanged icons

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,27 +5,14 @@
     >
       Contact.
     </h1>
-    <div>
-      <div class="flex items-center">
-        <Icon name="heroicons:envelope" />
-        <a
-          href="mailto:ridleyjal@gmail.com"
-          class="text-blue-600 underline visited:text-purple-600 hover:text-blue-800"
-          >Email</a
-        >
-      </div>
-    </div>
-    <div
-      class="grid grid-cols-3 gap-5 rounded-xl sm:gap-10 md:grid-cols-4 lg:gap-16"
-    >
+    <div class="gap-5 sm:gap-10 md:flex md:justify-center lg:gap-16">
       <NuxtLink
         to="https://github.com/jalridley"
         target="_blank"
         aria-label="Go to Jal's Github page"
         class="hover:text-yellow-500"
       >
-        <CardIcon icon="skill-icons:github-dark" title="Github" />
-        <!-- <CardIcon icon="skill-icons:github-light" title="Github" /> -->
+        <CardIcon icon="mdi:github" title="Github" />
       </NuxtLink>
       <NuxtLink
         to="https://www.linkedin.com/in/jal-ridley"
@@ -33,13 +20,15 @@
         aria-label="Go to Jal's LinkedIn profile"
         class="hover:text-yellow-500"
       >
-        <CardIcon icon="simple-icons:linkedin" title="LinkedIn" />
-        <!-- dark mode -->
-        <!-- <CardIcon
-          icon="simple-icons:linkedin"
-          title="LinkedIn"
-          color="#64748b"
-        /> -->
+        <CardIcon icon="mdi:linkedin" title="LinkedIn" />
+      </NuxtLink>
+      <NuxtLink
+        to="mailto:ridleyjal@gmail.com"
+        target="_blank"
+        aria-label="Email Jal"
+        class="hover:text-yellow-500"
+      >
+        <CardIcon icon="mdi:email-arrow-right" title="Email" />
       </NuxtLink>
     </div>
   </section>

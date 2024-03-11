a293d0cf82eb2bfab4e4313c983adf8da13c4c6f
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-11 15:13:03 +0100

    cssnano to nuxt config for netlify fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,10 @@ export default defineNuxtConfig({
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+      cssnano:
+        process.env.NODE_ENV === 'production'
+          ? { preset: ['default', { discardComments: { removeAll: true } }] }
+          : false, // disable cssnano when not in production
     },
   },
   modules: ['@nuxtjs/google-fonts', '@nuxt/image', 'nuxt-icon'],

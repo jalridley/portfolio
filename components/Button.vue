368af4cb3da2f6dd39f7407c64b620aeb514750a
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-12 22:56:14 +0100

    target prop to button

--- components/Button.vue
+++ components/Button.vue
@@ -1,6 +1,7 @@
 <template>
   <NuxtLink
     :to="to"
+    :target="target"
     :aria-label="ariaLabel"
     class="w-full rounded-xl bg-yellow-400 px-6 py-3 text-center text-xl hover:bg-yellow-500 lg:w-auto"
   >
@@ -10,6 +11,7 @@
 <script setup lang="ts">
 interface Props {
   to: string
+  target?: string
   ariaLabel: string
 }
 defineProps<Props>()

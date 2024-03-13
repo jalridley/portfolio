934e544683c9c8b1cbea3e0a8f04eadd585be54e
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-13 15:17:35 +0100

    card refactor

--- components/Card.vue
+++ components/Card.vue
@@ -1,17 +1,21 @@
 <template>
-  <article class="w-full overflow-hidden rounded-xl bg-white shadow-3xl">
-    <div class="h-72 md:h-80 lg:h-96">
-      <slot />
-    </div>
-
-    <div class="border-t p-5">
-      <h2 class="font-besley text-2xl font-bold capitalize md:text-3xl">
-        {{ header }}
-      </h2>
-      <p class="pt-3">
+  <article
+    class="flex w-full flex-1 flex-col justify-between overflow-hidden rounded-xl bg-white pb-3 shadow-3xl"
+  >
+    <div class="pb-5">
+      <div class="h-72 w-full md:h-80 lg:h-96">
+        <slot />
+      </div>
+      <div class="border-t p-5">
+        <h2 class="mb-3 font-besley text-2xl font-bold capitalize md:text-3xl">
+          {{ header }}
+        </h2>
         <slot name="text" />
-      </p>
+      </div>
     </div>
+    <span class="px-5 pb-5 text-center">
+      <slot name="button" />
+    </span>
   </article>
 </template>
 <script setup lang="ts">

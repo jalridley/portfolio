b7af33b2a196e4a0db63b19f0488ec8e67e48c08
Jal Ridley <72085091+jalridley@users.noreply.github.com>
2024-03-13 16:13:14 +0100

    block and lg:inline-block to button

--- components/Button.vue
+++ components/Button.vue
@@ -3,7 +3,7 @@
     :to="to"
     :target="target"
     :aria-label="ariaLabel"
-    class="w-full rounded-xl bg-yellow-400 px-6 py-3 text-lg hover:bg-yellow-500 lg:w-auto"
+    class="block rounded-xl bg-yellow-400 px-6 py-3 text-center text-lg hover:bg-yellow-500 lg:inline-block"
   >
     <slot />
   </NuxtLink>
